7517ed18b4e56bdada0aa00c9c598b8fdff3cf37
Shuudy <ancieux.jerome059@gmail.com>
2023-12-09 18:52:52 +0100

    Add section

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,4 +4,13 @@
 <x-navbar/>
 <x-hero/>
 
+<section class="section">
+    <div class="section-content">
+        <div class="section-title">
+            <h2>Articles récents</h2>
+        </div>
+        hello
+    </div>
+</section>
+
 @endsection
