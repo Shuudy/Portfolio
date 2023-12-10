826e243f594014ab70b307fafe06acbc884c708e
Shuudy <ancieux.jerome059@gmail.com>
2023-12-10 15:04:36 +0100

    Creation of footer

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,12 @@
+<footer>
+    <div class="footer-content">
+        <div class="footer-left">&copy; 2023 Mon portfolio. Tous droits réservés.</div>
+        <div class="footer-right">
+            <div class="social-links">
+                <a href="#" target="_blank">Facebook</a>
+                <a href="#" target="_blank">Twitter</a>
+                <a href="#" target="_blank">LinkedIn</a>
+            </div>
+        </div>
+    </div>
+</footer>
