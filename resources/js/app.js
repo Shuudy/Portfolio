b0dd49339516f9a0904e430a994edac013a442eb
Shuudy <ancieux.jerome059@gmail.com>
2023-12-10 15:04:24 +0100

    Add change height of navbar when scrollY

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,2 +1,18 @@
 
 import '../css/app.scss';
+
+
+const navbar = document.querySelector('.navbar');
+
+function handleScroll() {
+    const scrollY = window.scrollY;
+    if (scrollY > 100) {
+        navbar.classList.add('scrolled');
+    } else {
+        navbar.classList.remove('scrolled');
+    }
+}
+
+window.addEventListener('scroll', handleScroll);
+
+document.addEventListener('DOMContentLoaded', handleScroll);
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -12,7 +12,7 @@
         <div class="card-list">
             @for($i=0;$i<3;$i++)
             <div class="card">
-                <div class="card-image"><a href=""><img src="https://dudeproducts.com/cdn/shop/articles/gigachad_1024x1024.jpg?v=1667928905" alt=""></a></div>
+                <div class="card-image"><a href=""><img src="https://i.ibb.co/b30WqV8/408005283-258814217207388-8741290683313476433-n-1.jpg" alt=""></a></div>
                 <div class="card-content">                 
                     <h2 class="card-title"><a href="">Laporte, dans le meilleur des mondes</a></h2>
                     <div class="card-text">Après les premières victoires en World Tour en 2022, Christophe Laporte a remporté ses premières grandes courses d’un jour en 2023. Vainqueur de Gand-Wevelgem, d’A Travers la Flandre et champion</div>
@@ -26,4 +26,5 @@
     </div>
 </section>
 
+<x-footer/>
 @endsection
