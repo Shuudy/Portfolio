8f86889ddded14709208a0fb0e90fdc13208a8ec
Shuudy <ancieux.jerome059@gmail.com>
2023-12-31 20:16:56 +0100

    Remove realisations show initial template

--- resources/views/realisations/index.blade.php
+++ resources/views/realisations/index.blade.php
@@ -10,22 +10,7 @@
 <x-navbar/>
 <div class="navbar-skip"></div>
 
-<div class="realisation-list">  
-    @for($i=0;$i<6;$i++)
-    <div class="realisation">
-        <div class="realisation-image">
-            <a href=""><img src="https://i.ibb.co/QNmSXkh/273573669-990160091619911-7621325696563393785-n.jpg" alt=""></a>
-        </div>
-        <div class="realisation-content">
-            <div class="realisation-subtitle">Publié dans Vélo Magazine</div>
-            <h2 class="realisation-title"><a href="">Laporte, dans le meilleur des mondes</a></h2>
-            <div class="realisation-desc">
-                <p>Après les premières victoires en World Tour en 2022, Christophe Laporte a remporté ses premières grandes courses d’un jour en 2023. Vainqueur de Gand-Wevelgem, d’A Travers la Flandre et champion</p>
-            </div>
-        </div>
-    </div>
-    @endfor
-
+<div class="realisation-list">
     @foreach($realisations as $realisation)
     <div class="realisation">
         <div class="realisation-image">
@@ -37,7 +22,7 @@
             <div class="realisation-desc"><p>{{ Str::limit(strip_tags($realisation->content), 200) }}</p></div>
         </div>
     </div>
-    @endforeach    
+    @endforeach
 </div>
 
 <x-footer/>
