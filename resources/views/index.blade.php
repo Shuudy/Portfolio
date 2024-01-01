3474ea2eba5e1a560c3e54de1d19e9694fe79b44
Shuudy <ancieux.jerome059@gmail.com>
2024-01-01 18:37:50 +0100

    Add about me section in home page

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,7 +4,28 @@
 <x-navbar/>
 <x-hero/>
 
-<section class="section">
+<section class="section section-about">
+    <div class="section-content">
+        <div class="section-about-image">
+            <img src="https://team-break.fr/wp-content/uploads/2023/07/jeux-societe-horreur-1024x1024.png" alt="Image about me">
+        </div>        
+        <div class="section-about-text">
+            <div class="section-about-text-title">
+                <h2>Qui suis-je ?</h2>
+            </div>
+            <div class="section-about-text-content">
+                <p>Je suis un <b>développeur</b> passionné, spécialisé dans les technologies de l'information. Actuellement en <b>BTS Services Informatiques aux Organisations à Lille</b>, je m'efforce constamment de perfectionner mes compétences et d'explorer de nouvelles avenues dans le monde du <b>développement</b>.</p>
+                <p>Collaborant régulièrement avec des projets innovants, j'ai eu l'occasion de travailler sur des applications web et des systèmes complexes. Mon engagement envers <b>l'excellence technique</b> se reflète dans mes projets.</p>
+                <p>J'ai également acquis une solide expérience en programmation dans divers langages, et je maîtrise particulièrement les défis liés au <b>développement web</b>. En parallèle de mes études, je participe activement à des projets open source et à des compétitions de programmation.</p>
+            </div>
+            <div class="section-btn">
+                <a href="" class="btn btn-section">Voir mon CV</a>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="section background">
     <div class="section-content">
         <div class="section-title">
             <h2>Articles récents</h2>
