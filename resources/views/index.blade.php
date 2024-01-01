5a036803f401409a9dea03b3da4592552a3e5bf3
Shuudy <ancieux.jerome059@gmail.com>
2024-01-01 18:38:43 +0100

    Add image alt for last realisations

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -33,7 +33,7 @@
         <div class="card-list">
             @foreach($lastRealisations as $realisation)
             <div class="card">
-                <div class="card-image"><a href="{{ route('realisations.show', ['slug' => $realisation->slug, 'id' => $realisation->id]) }}"><img src="{{ asset('storage/' . $realisation->image) }}" alt=""></a></div>
+                <div class="card-image"><a href="{{ route('realisations.show', ['slug' => $realisation->slug, 'id' => $realisation->id]) }}"><img src="{{ asset('storage/' . $realisation->image) }}" alt="Realisation image"></a></div>
                 <div class="card-content">                 
                     <h2 class="card-title"><a href="{{ route('realisations.show', ['slug' => $realisation->slug, 'id' => $realisation->id]) }}">{{ $realisation->title }}</a></h2>
                     <div class="card-subtitle home">{{ $realisation->subtitle }}</div>
