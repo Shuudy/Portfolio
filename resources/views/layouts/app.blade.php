3a470ae89a5c09f60082092fc73d243d5b7dbb17
Shuudy <ancieux.jerome059@gmail.com>
2024-01-21 19:51:56 +0100

    Change separator in html title

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>@yield('title', 'Accueil') | {{ env('WEBAPP_NAME') }}</title>
+    <title>@yield('title', 'Accueil') - {{ env('WEBAPP_NAME') }}</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@tabler/icons-webfont@latest/tabler-icons.min.css">
     @vite(['resources/js/app.js'])
 </head>
