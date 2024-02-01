3591ce0987c7d6cef7ec81fe81645d4f8295701f
Shuudy <ancieux.jerome059@gmail.com>
2024-02-01 18:33:11 +0100

    Change tomselect dropdown background color when hover element

--- resources/views/admin/realisations/edit.blade.php
+++ resources/views/admin/realisations/edit.blade.php
@@ -68,6 +68,12 @@
 <link href="https://cdn.jsdelivr.net/npm/tom-select@2.3.1/dist/css/tom-select.css" rel="stylesheet">
 <script src="https://cdn.jsdelivr.net/npm/tom-select@2.3.1/dist/js/tom-select.complete.min.js"></script>
 
+<style>
+    .ts-dropdown .active {
+        background-color: #f1f1f1;
+    }
+</style>
+
 <script>
 new TomSelect('#subskills', {
     plugins: {
