eaf25ddd8a1c4410f9ec0d1b92943432008a075e
Shuudy <ancieux.jerome059@gmail.com>
2024-02-26 20:00:46 +0100

    Edit contact mail title

--- resources/views/emails/contact.blade.php
+++ resources/views/emails/contact.blade.php
@@ -64,7 +64,7 @@
 </head>
 <body>
     <div class="container">
-        <h1>New contact form submission from {{ $formData['name'] }}</h1>
+        <h1>Nouveau formulaire de contact de {{ $formData['name'] }}</h1>
         <div class="mail-info-container">
             <div class="mail-info">
                 <div class="mail-info-icon">
