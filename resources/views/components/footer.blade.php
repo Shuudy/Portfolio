2e50804dd99dd36e2205c654a6a2e03aeb1b576d
Shuudy <ancieux.jerome059@gmail.com>
2024-04-07 15:17:40 +0200

    Change social networks facebook and twitter by Github in the footer

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -3,8 +3,7 @@
         <div class="footer-left">&copy; 2023 {{ env('WEBAPP_NAME') }}. Tous droits réservés.</div>
         <div class="footer-right">
             <div class="social-links">
-                <a href="#" target="_blank">Facebook</a>
-                <a href="#" target="_blank">Twitter</a>
+                <a href="https://github.com/Shuudy" target="_blank">Github</a>
                 <a href="https://www.linkedin.com/in/j%C3%A9r%C3%B4me-ancieux" target="_blank">LinkedIn</a>
             </div>
         </div>
