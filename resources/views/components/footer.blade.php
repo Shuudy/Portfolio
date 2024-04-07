dfc75efb125ae4da92eb628f9b7ca691c2399035
Shuudy <ancieux.jerome059@gmail.com>
2024-04-07 15:19:01 +0200

    Change copyright date from 2023 to 2024

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,6 +1,6 @@
 <footer>
     <div class="footer-content">
-        <div class="footer-left">&copy; 2023 {{ env('WEBAPP_NAME') }}. Tous droits réservés.</div>
+        <div class="footer-left">&copy; 2024 {{ env('WEBAPP_NAME') }}. Tous droits réservés.</div>
         <div class="footer-right">
             <div class="social-links">
                 <a href="https://github.com/Shuudy" target="_blank">Github</a>
